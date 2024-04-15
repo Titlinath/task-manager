fc0d783619f41c13e1cb7f9bc1e55154577f16eb
Titlinath <titlinath3@gmail.com>
2024-04-15 23:05:47 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,37 @@
+const Todo = {
+    id:   2,
+    content: "So what kind of art you want to see ?",
+    status: "active"
+}
+
+const AllTodo = {
+    //not_started : {
+        1:{
+        name: "Not Started",
+        todos : [
+            {
+                id:   2,
+                content: "So what kind of art you want to see ?"
+            },
+            ]
+    },
+    //in_progress : {
+        2: {
+        name: "In Progress",
+        todos : [
+            {
+                id:   2,
+                content: "So what kind of art you want to see ?"
+            },
+        ]
+    },
+    //completed : {
+        3: {
+        name: "Completed",
+        todos : [
+            {
+                id:   2,
+                content: "So what kind of art you want to see ?"
+            },
+        ]
+}
